efb72037aaead2e77b4a376e80a0529c7af55aee
kd-kinuthiadvid <kd.kinuthiadavid@gmail.com>
2018-06-04 10:42:42 +0300

    add html and typescript

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {GithubService} from '../github/github.service';
+
 
 @Component({
   selector: 'app-account',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountComponent implements OnInit {
 
-  constructor() { }
+  user: any=[];
+  repos: any=[];
+  username:string;
+  constructor(private _githubService: GithubService) {
+  this._githubService.getUser().subscribe(user => {
+    console.log)(user);
+    this.user = user;
+  })
+}
+this._githubService.getRepos().subscribe(repos =>{
+  this,repos = repos;
+})
 
   ngOnInit() {
+  }
+  searchUser(){
+    this._githubService.updateUser(this.username);
+    this._githubService.getUser().subscribe(user => {
+      this.user = user;
+    })
+    this._githubService.getRepos().subscribe(repos => {
+      this.repos = repos;
+    })
   }
 
 }
