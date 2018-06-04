e04d24447843f544d81258be2e883566e508f0e1
kd-kinuthiadvid <kd.kinuthiadavid@gmail.com>
2018-06-04 11:16:09 +0300

    define app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'
+import { AlertsService } from './alert-service/alerts.service'
 import { AppComponent } from './app.component';
-import { AccountComponent } from './account/account.component';
+import { accountComponent } from './account/account.component';
+import { GithubService } from './github/github.service';
+
 
 @NgModule({
   declarations: [
@@ -10,9 +14,11 @@ import { AccountComponent } from './account/account.component';
     AccountComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [GithubService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
