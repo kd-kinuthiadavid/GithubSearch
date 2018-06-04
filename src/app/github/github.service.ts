9151d77577fd27d8d1489ff945609621e148a135
kd-kinuthiadvid <kd.kinuthiadavid@gmail.com>
2018-06-04 10:58:13 +0300

    add typescript to github service

--- src/app/github/github.service.ts
+++ src/app/github/github.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import {HttpClient}  from '@angular/common/http';
+import {User} from '../user'
+import {Repos} from '../repos'
+import {environment} from '../../environments/environment'
+import 'rxjs/add/operator/map'
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class GithubService {
+ private apiUrl :string = environment.apiUrl;
+  username:string;
 
-  constructor() { }
+  constructor(private _http: HttpClient){
+    console.log('Github Service Ready...');
+    this.username =  'garvinipkiss';
+
+  }
+  getUser(){
+    return this._http.get("https://api.github.com/users/"+this.username+'?access_token='+this.apiUrl)
+    .map(result=>result)
+
+  }
+  getRepos(){
+    return this._http.get("https://api.github.com/users/"+this.username+'/repos'+'?access_token='+this.apiUrl)
+    .map(result=>result)
+}
+updateUser(username:string){
+  this.username = username;
+}
 }
